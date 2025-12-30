25ff66129f6775ea82343d32e93efb10c11eb5c4
Császár Mátyás [Vader] <vaderhun@gmail.com>
2025-12-30 16:38:31 +0100

    Fix and test std::array<T, 0>

--- include/vide/types/array.hpp
+++ include/vide/types/array.hpp
@@ -11,7 +11,7 @@ namespace vide { // ------------------------------------------------------------
 template <class Archive, class T, std::size_t N>
 inline void VIDE_FUNCTION_NAME_SERIALIZE(Archive& ar, std::array<T, N>& array) {
 	if constexpr (Archive::template supports_binary<T>) {
-		ar(binary_data(array.data(), sizeof(array)));
+		ar(binary_data(array.data(), N*sizeof(T)));
 	} else {
 		for (auto& i : array)
 			ar(i);
